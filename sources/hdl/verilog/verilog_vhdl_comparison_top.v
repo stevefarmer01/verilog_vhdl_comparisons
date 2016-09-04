89072717da4f6accb6f5d4b2952ac945f11bde46
Steve Farmer <stephenrfarmer@hotmail.com>
2016-09-05 00:41:10 +0100

    Difference in verilog and vhdl code bin files

--- sources/hdl/verilog/verilog_vhdl_comparison_top.v
+++ sources/hdl/verilog/verilog_vhdl_comparison_top.v
@@ -1,86 +1,73 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03.03.2016 13:25:16
--- Design Name: 
--- Module Name: level_change_domain_top - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
+//  Company: 
+//  Engineer:
+//  
+//  Create Date: 03.03.2016 13:25:16
+//  Design Name: 
+//  Module Name: level_change_domain_top - Behavioral
+//  Project Name: 
+//  Target Devices: 
+//  Tool Versions: 
+//  Description: 
+//  
+//  Dependencies: 
+//  
+//  Revision:
+//  Revision 0.01 - File Created
+//  Additional Comments:
+//  
+//  --------------------------------------------------------------------------------
+
+module verilog_vhdl_comparison_top (signal_in_bit, SystemClk, signal_out_bit);
+
+  //signal_in : in  std_logic_vector(3 downto 0);
+  input signal_in_bit;
+  input SystemClk;
+  //signal_out : out  std_logic_vector(3 downto 0);
+  output signal_out_bit;
+
+  parameter number_of_domain_cross_regs = 3; // must be natural number
+
+  reg signal_in_bit_r = 1'b0;
+  reg signal_out_bit = 1'b0;
+  wire signal_out_bit_s = 1'b0;
+
+//component level_change_domain
+//    generic(number_of_domain_cross_regs : natural := 2);
+//    Port ( signal_in : in  std_logic_vector;
+//           SystemClk : in  std_logic;
+//           signal_out : out  std_logic_vector
+//           );
+//end component;
 
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+//initial
 
-use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity verilog_vhdl_comparison_top is
-    generic(number_of_domain_cross_regs : natural := 3);
-    Port ( 
-           --signal_in : in  std_logic_vector(3 downto 0);
-           signal_in_bit : in  std_logic;
-           SystemClk : in  std_logic;
-           --signal_out : out  std_logic_vector(3 downto 0);
-           signal_out_bit : out  std_logic
-           );
-end verilog_vhdl_comparison_top;
-
-architecture Behavioral of verilog_vhdl_comparison_top is
-
---component level_change_domain
---    generic(number_of_domain_cross_regs : natural := 2);
---    Port ( signal_in : in  std_logic_vector;
---           SystemClk : in  std_logic;
---           signal_out : out  std_logic_vector
---           );
---end component;
-
-signal signal_in_bit_r, signal_out_bit_s : std_logic := '0';
-
-begin
-
-in_reg_proc : process
+always @(posedge SystemClk)
 begin 
-  wait until rising_edge(SystemClk);
   signal_in_bit_r <= signal_in_bit;
-end process;
+end
 
 
---inst_level_change_domain_bit : level_change_domain
---    generic map(
---            number_of_domain_cross_regs => number_of_domain_cross_regs -- : natural := 2
---            )
---    Port map( 
---            signal_in(0) => signal_in_bit_r,  -- : in  std_logic_vector;
---            SystemClk => SystemClk,         -- : in  std_logic;
---            signal_out(0) => signal_out_bit_s -- : out  std_logic_vector
---           );
+//inst_level_change_domain_bit : level_change_domain
+//    generic map(
+//            number_of_domain_cross_regs => number_of_domain_cross_regs -- : natural := 2
+//            )
+//    Port map( 
+//            signal_in(0) => signal_in_bit_r,  -- : in  std_logic_vector;
+//            SystemClk => SystemClk,         -- : in  std_logic;
+//            signal_out(0) => signal_out_bit_s -- : out  std_logic_vector
+//           );
 
-signal_out_bit_s <= signal_in_bit_r;
+assign signal_out_bit_s = signal_in_bit_r;
 
-out_reg_proc : process
+always @(posedge SystemClk)
 begin 
-  wait until rising_edge(SystemClk);
   signal_out_bit <= signal_out_bit_s;
-end process;
+end
 
---signal_out_bit <= signal_out_bit_r;
+//signal_out_bit <= signal_out_bit_r;
 
---mult_output <= unsigned(input_a) * unsigned(input_b);
+//mult_output <= unsigned(input_a) * unsigned(input_b);
 
-end Behavioral;
+endmodule
